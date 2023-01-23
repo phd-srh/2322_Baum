622d5ae7cf150f108d0bde26f67014c449d0675c
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-01-23 11:59:40 +0100

    Der wehende Baum ...

--- src/Baum.java
+++ src/Baum.java
@@ -1,2 +1,12 @@
 public class Baum {
+    private int ästeanzahl;
+
+    public Baum(int ästeanzahl) {
+        this.ästeanzahl = ästeanzahl;
+    }
+
+    public void wehen() {
+        System.out.println("Es weht der Baum mit " + ästeanzahl + " Ästen.");
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Baum henry = new Baum(214);
+        Baum hillary = new Baum(4012);
+
+        henry.wehen();
+        hillary.wehen();
     }
 }
